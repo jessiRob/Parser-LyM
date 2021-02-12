31879033c49484f6519f6ff9171d8096025f6d38
jessiRob <69654419+jessiRob@users.noreply.github.com>
2021-02-11 20:00:30 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,68 +2,6 @@ def leerArchivo (ruta_archivo):
     archivo= open(ruta_archivo,"r")
     linea=archivo.readline()
     texto = []
-
-    while (len(linea)!=0):
-        open1 = 0
-        close = 0
-        #texto = ""
-        while (open1 != close) and (len(linea)!=0):
-            open1 += linea.count("(")
-            close += linea.count(")")
-            linea=archivo.readline()
-        if (open1 == close):
-            texto.append(linea.replace('\n',''))
-            linea=archivo.readline()
-    
-    archivo.close         
-    print(texto)
-    return texto
-
-def analizarArchivo(ruta_archivo):
-    texto = leerArchivo (ruta_archivo)
-    linea  = 0
-    correcto = True
-    variables = []
-    functions = []
-
-    if texto[linea][0] != "(":
-        correcto = False
-
-    while correcto and (linea<len(texto)):
-        instruccion = texto[linea][:texto[linea].find("(")]
-        if instruccion.contains("define"):
-            pass
-        elif instruccion.contains("block"):
-            pass
-        elif instruccion.contains("if"):
-            pass
-        elif instruccion.contains("walk"):
-            pass
-        elif instruccion.contains("rotate"):
-            pass
-        elif instruccion.contains("look"):
-            pass
-        elif instruccion.contains("drop"):
-            pass
-        elif instruccion.contains("free"):
-            pass
-        elif instruccion.contains("pick"):
-            pass
-        elif instruccion.contains("grab"):
-            pass
-        elif instruccion.contains("walkTo"):
-            pass
-        elif instruccion.contains("NOP"):
-            pass
-        #elif texto[linea][0:-1] in :
-            #pass
-
-
-"""
-def leerArchivo (ruta_archivo):
-    archivo= open(ruta_archivo,"r")
-    linea=archivo.readline()
-    texto = []
     textoStr=""
 
     while (len(linea)!=0):
@@ -83,12 +21,11 @@ def leerArchivo (ruta_archivo):
                 #print("x",open1, close)
             elif (textoStr[s] != " ") and (open1 == 0):
                 #print("F")
-                return texto
+                return []
             s +=1
         while (open1 != close) and (s<(len(textoStr))):
             if textoStr[s] == "(":
                 open1 +=1
-                
             if textoStr[s] == ")":
                 close +=1
                 #print(open1,close)
@@ -97,11 +34,79 @@ def leerArchivo (ruta_archivo):
                     close = 0
                     texto.append(textoStr[openPos:s+1])
                     openPos = 0
-                #print(texto,textoStr[s] ,s, openPos, open1, close)    
+                #print(texto,textoStr[s] ,s, openPos, open1, close)  
             s +=1
-
     archivo.close         
     return texto
-"""
 
-print(leerArchivo("D:\datos\Jessica\jess\sistemas\lym\Proyecto1\PruebaBien1.txt"))
+def separarPalabra(inicio, instruccion):
+    i=inicio
+    palabra = ""
+    while (i < len(instruccion)-1) and (palabra ==""):
+        if (instruccion[i] == " ") or (instruccion[i] == "("):
+            print("1", instruccion[i])
+            i+=1
+        else:
+            find1 = instruccion[i:].find(" ")
+            find2 = instruccion[i:].find("(")
+            if (find1 > find2) and (find2 != -1):
+                palabra = instruccion[i:find2+i]
+            elif (find1 == -1) and (find2 ==-1):
+                return False
+            else:
+                palabra = instruccion[i:find1+i]
+            print("2", instruccion[i], find1, find2)
+    print("fijo", palabra)
+    return palabra
+
+def analizarArchivo(ruta_archivo):
+    texto = leerArchivo (ruta_archivo)
+    print(texto)
+    comando  = 0
+    correcto = True
+    variables = []
+    functions = []
+
+    if texto[comando][0][0] != "(":
+        correcto = False
+
+    while correcto and (comando<len(texto)):
+        instruccion = texto[comando]
+        palabra = separarPalabra(1, instruccion)
+        if palabra.contains("define"):
+            pass
+        elif palabra.contains("block"):
+            pass
+        elif palabra.contains("if"):
+            pass
+        elif palabra.contains("walk"):
+            pass
+        elif palabra.contains("rotate"):
+            pass
+        elif palabra.contains("look"):
+            pass
+        elif palabra.contains("drop"):
+            pass
+        elif palabra.contains("free"):
+            pass
+        elif palabra.contains("pick"):
+            pass
+        elif palabra.contains("grab"):
+            pass
+        elif palabra.contains("walkTo"):
+            pass
+        elif palabra.contains("NOP"):
+            pass
+        #elif texto[linea][0:-1] in :
+            #pass
+
+        comando += 1
+    return correcto
+
+
+
+
+
+
+print(analizarArchivo("D:\datos\Jessica\jess\sistemas\lym\Proyecto1\PruebaBien1.txt"))
+print("ya")
