6bef80da42ff0d1e8f1dce320819550ade58e638
Luisa Fuentes <l.fuentesl@uniandes.edu.co>
2021-02-14 11:16:23 -0500

    adds

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+
+
 def leerArchivo (ruta_archivo):
     archivo= open(ruta_archivo,"r")
     linea=archivo.readline()
@@ -5,6 +7,9 @@ def leerArchivo (ruta_archivo):
     textoStr=""
 
     while (len(linea)!=0):
+        textoStr=textoStr.replace("    ","")#ADDED
+        textoStr=textoStr.replace("("," ( ")#ADDED
+        textoStr=textoStr.replace(")"," ) ")#ADDED
         textoStr += linea.replace("\n","")
         linea=archivo.readline()
     #print(textoStr)
@@ -21,11 +26,11 @@ def leerArchivo (ruta_archivo):
                 #print("x",open1, close)
             elif (textoStr[s] != " ") and (open1 == 0):
                 #print("F")
-                return []
+                return texto
             s +=1
         while (open1 != close) and (s<(len(textoStr))):
             if textoStr[s] == "(":
-                open1 +=1
+                open1 +=1               
             if textoStr[s] == ")":
                 close +=1
                 #print(open1,close)
@@ -34,79 +39,58 @@ def leerArchivo (ruta_archivo):
                     close = 0
                     texto.append(textoStr[openPos:s+1])
                     openPos = 0
-                #print(texto,textoStr[s] ,s, openPos, open1, close)  
+                #print(texto,textoStr[s] ,s, openPos, open1, close)    
             s +=1
+
     archivo.close         
     return texto
 
-def separarPalabra(inicio, instruccion):
-    i=inicio
-    palabra = ""
-    while (i < len(instruccion)-1) and (palabra ==""):
-        if (instruccion[i] == " ") or (instruccion[i] == "("):
-            print("1", instruccion[i])
-            i+=1
-        else:
-            find1 = instruccion[i:].find(" ")
-            find2 = instruccion[i:].find("(")
-            if (find1 > find2) and (find2 != -1):
-                palabra = instruccion[i:find2+i]
-            elif (find1 == -1) and (find2 ==-1):
-                return False
-            else:
-                palabra = instruccion[i:find1+i]
-            print("2", instruccion[i], find1, find2)
-    print("fijo", palabra)
-    return palabra
-
 def analizarArchivo(ruta_archivo):
     texto = leerArchivo (ruta_archivo)
-    print(texto)
-    comando  = 0
+    linea  = 0
     correcto = True
-    variables = []
+    lista_p = [1]
     functions = []
 
-    if texto[comando][0][0] != "(":
+    if texto[linea][0] != "(":
         correcto = False
 
-    while correcto and (comando<len(texto)):
-        instruccion = texto[comando]
-        palabra = separarPalabra(1, instruccion)
-        if palabra.contains("define"):
+    for comando in texto:
+        comando=comando.split() 
+        parenthesis_o=comando.count("(")
+        parenthesis_c=comando.count(")")
+        
+        print(comando,parenthesis_o, parenthesis_c)
+            
+"""
+    
+        if instruccion.contains("define"):
             pass
-        elif palabra.contains("block"):
+        elif instruccion.contains("block"):
             pass
-        elif palabra.contains("if"):
+        elif instruccion.contains("if"):
             pass
-        elif palabra.contains("walk"):
+        elif instruccion.contains("walk"):
             pass
-        elif palabra.contains("rotate"):
+        elif instruccion.contains("rotate"):
             pass
-        elif palabra.contains("look"):
+        elif instruccion.contains("look"):
             pass
-        elif palabra.contains("drop"):
+        elif instruccion.contains("drop"):
             pass
-        elif palabra.contains("free"):
+        elif instruccion.contains("free"):
             pass
-        elif palabra.contains("pick"):
+        elif instruccion.contains("pick"):
             pass
-        elif palabra.contains("grab"):
+        elif instruccion.contains("grab"):
             pass
-        elif palabra.contains("walkTo"):
+        elif instruccion.contains("walkTo"):
             pass
-        elif palabra.contains("NOP"):
+        elif instruccion.contains("NOP"):
             pass
         #elif texto[linea][0:-1] in :
             #pass
-
-        comando += 1
-    return correcto
+"""
 
 
-
-
-
-
-print(analizarArchivo("D:\datos\Jessica\jess\sistemas\lym\Proyecto1\PruebaBien1.txt"))
-print("ya")
+print(leerArchivo("PruebaBien1.txt"))
